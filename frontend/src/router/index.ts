4e95c0eedd5ce2d1c42d59f28d1782d0352515c4
Erik Tomisaki Rodrigues da Silva <erik.trs01@gmail.com>
2025-07-24 15:39:58 -0300

    Added Vue Router configuration

---
+++ frontend/src/router/index.ts
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from "vue-router";
+import ProductsView from "../views/ProductsView.vue";
+import CouponsView from "../views/CouponsView.vue";
+import OrdersView from "../views/OrdersView.vue";
+
+const routes = [
+  { path: "/", redirect: "/products" },
+  { path: "/products", component: ProductsView },
+  { path: "/coupons", component: CouponsView },
+  { path: "/orders", component: OrdersView },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
