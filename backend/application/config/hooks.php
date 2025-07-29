9a3a7641acc20084b22886ceeb275d0b3800e8e8
Erik Tomisaki Rodrigues da Silva <erik.trs01@gmail.com>
2025-07-29 20:24:18 -0300

    Changed hook activation from post controller constructor to pre system

--- backend/application/config/hooks.php
+++ backend/application/config/hooks.php
@@ -12,9 +12,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$hook['post_controller_constructor'][] = array(
+$hook['pre_system'][] = array(
     'class'    => 'CorsHook',
     'function' => 'handle',
     'filename' => 'CorsHook.php',
     'filepath' => 'hooks'
 );
+
