576ef44a921c17cac78a0f6262f7d31153da8fd6
Erik Tomisaki Rodrigues da Silva <erik.trs01@gmail.com>
2025-08-01 23:25:15 -0300

    Added Cart route in router configuration

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,12 +2,14 @@ import { createRouter, createWebHistory } from "vue-router";
 import ProductsView from "../views/ProductsView.vue";
 import CouponsView from "../views/CouponsView.vue";
 import OrdersView from "../views/OrdersView.vue";
+import CartView from "../views/CartView.vue";
 
 const routes = [
   { path: "/", redirect: "/products" },
   { path: "/products", component: ProductsView },
   { path: "/coupons", component: CouponsView },
   { path: "/orders", component: OrdersView },
+  { path: "/cart", component: CartView },
 ];
 
 const router = createRouter({
